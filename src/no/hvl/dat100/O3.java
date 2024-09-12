6f20ade323bb77933fbe3916235164f5d39964a8
h184920 <itunesglambek@gmail.com>
2024-09-13 01:46:21 +0200

    Update O3.java

--- src/no/hvl/dat100/O3.java
+++ src/no/hvl/dat100/O3.java
@@ -3,20 +3,56 @@ package no.hvl.dat100;
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.*;
 
+
 public class O3 {
 	
 	public static void main(String[] args) {
 		
 		int n = parseInt(showInputDialog("Skriv in en heltallsverdi for n; "));
-		showMessageDialog(null, "" + n + "! = " + fakN(n));
 		
+		showMessageDialog(null, "" + n + "! = " + nFak(n));
 	}
 	
-	public static int fakN(int n) {
-		if (n <= 1) {
-			return n;
-		}
-		return n*fakN(n - 1);
+	
+	public static int nFak(int n) {
+		
+		if (n <= 1) return 1;
+		
+		return nFak(n - 1)*n;
 	}
+	
 
-}
+	
+	public static int fncci(int n) {
+		
+		if (n <= 2) return 1;
+		
+		return fncci(n - 2) + fncci(n - 1);
+	}
+	
+	
+	public static int tn(int n) {
+		
+		if (n <= 1) return 1;
+		
+		return tn(n - 1) + n;
+	}
+	
+	
+	public static int tnS(int n) {
+		
+		if (n < 1) return 0;
+		
+		if (n == 1) return 1;
+		
+		return 2*tnS(n - 1) - tnS(n - 2) + n;
+	}
+	
+
+	}
+	
+
+
+
+
+
